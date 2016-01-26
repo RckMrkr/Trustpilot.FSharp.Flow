be7f0d00ebba4eb2f26786bae508baf63fb763d2
appveyor <appveyor>
2016-01-26 09:56:09 +0000

    Bump version to 0.2

--- src/Trustpilot.FSharp.Flow/AssemblyInfo.fs
+++ src/Trustpilot.FSharp.Flow/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Trustpilot.FSharp.Flow")>]
 [<assembly: AssemblyProductAttribute("Trustpilot.FSharp.Flow")>]
 [<assembly: AssemblyDescriptionAttribute("A library for building contract based execution flows")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] Version = "0.2"
